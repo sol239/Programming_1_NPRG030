b2c692c0a884baf38afbfb383cf28f77d9042c25
david_valek17 <david.valek17@gmail.com>
2023-12-18 23:25:09 +0100

    Final Commit:   - solving with catalonan number

--- (hw07) All correct bracketings/main.py
+++ (hw07) All correct bracketings/main.py
@@ -1,3 +1,21 @@
 # handle input
+from math import comb
 n = int(input())
 
+def catalonan_1(n):
+    """Solution using catalonan numbers computed by recursion"""
+    if n == 0:
+        return 1
+    else:
+        return int((4*n-2)/(n+1)*catalonan_1(n-1))
+
+#print(catalonan_1(n))
+
+def catalonan_2(n):
+    """Solution using catalonan numbers computed by combinatorics"""
+    if n == 0:
+        return 1
+    else:
+        return(int((1 / (n + 1)) * comb(2 * n, n)))
+
+print(catalonan_2(n))
